70e9638a1787b8b3a65717cbc52966c22ac96bd8
chiragdhameja <chiragdhameja@gmail.com>
2024-02-14 21:28:11 -0500

    sanity connection

---
+++ sanity/schemaTypes/index.ts
@@ -0,0 +1,14 @@
+import experience from './experience'
+import pageInfo from './pageInfo'
+import project from './project'
+import skills from './skills'
+import socials from './socials'
+
+export const schemaTypes = [
+    pageInfo,
+    experience,
+    project,
+    skills,
+    socials
+    
+]
